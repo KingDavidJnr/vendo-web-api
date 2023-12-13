9bef24f2a4e7873729bbeb4a52580dc7ff059a66
David Oduse <odusedavid@gmail.com>
2023-12-13 02:38:54 +0100

    feat: Add models for users, vendors, customers, products, orders, and reviews.

---
+++ model/customerModel.js
@@ -0,0 +1,16 @@
+// model/customer/customerModel.js
+const mongoose = require('mongoose');
+const User = require('../user/userModel');
+
+const customerSchema = new mongoose.Schema({
+  // Additional customer-related fields
+  purchases: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  }],
+}, { timestamps: true });
+
+// Extend the User model using Mongoose discriminators
+const CustomerModel = User.discriminator('Customer', customerSchema);
+
+module.exports = CustomerModel;

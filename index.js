990d0cb4b2cf9fdb3b2574d7a7bdb6799e1b657e
David Oduse <odusedavid@gmail.com>
2023-12-17 18:23:36 +0100

    feat: update port number to ENV file in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
